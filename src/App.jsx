de2d88ec54b7adb37766a6916b230a65e8f95d27
Krishnendu Dasgupta <krishnendu.kdg@gmail.com>
2023-08-12 23:20:03 +0530

    no merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -20,12 +20,11 @@ function App() {
       <Route path="/" element={<HomePage />} />
 
       <Route path="/" element={<MainLayout />}>
-<<<<<<< HEAD
-=======
+
         {/* These will later be wrapped inside a Protected Route */}
         <Route path="rooms" element={<Rooms />} />
         <Route path="rooms/create" element={<h1>Create a Room Page</h1>} />
->>>>>>> 6e28b3aab82dc21daa70c97302d1180d47ef836d
+
 
         <Route path="rooms" element={<Rooms />} />
         <Route path="/room/:id" element={<RoomLayout />}>
